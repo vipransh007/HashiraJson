41a83c4bcd56f1bae4d64cb083d81b1d23d43966
Vipransh_Anand23152791 <128489392+vipransh007@users.noreply.github.com>
2025-08-28 11:21:21 +0530

    Update TestCase.java

--- Hashira/src/main/java/org/example/TestCase.java
+++ Hashira/src/main/java/org/example/TestCase.java
@@ -1,10 +1,4 @@
 public class TestCase {
-    double x;
-    double y;
-
-    // This part is optional but helpful for debugging
-    @Override
-    public String toString() {
-        return "TestCase(x=" + x + ", y=" + y + ")";
-    }
+    String base;
+    String value;
 }
